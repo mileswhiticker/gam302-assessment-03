32facbb9f0c72dc8c365e97b701a977646b48051
mwhiticker <miles_whiticker@gmail.com>
2012-05-10 05:24:29 +1000

    some files
    Signed-off-by: mwhiticker <miles_whiticker@gmail.com>

--- SpaceEconSim/SceneTraversals.cpp
+++ SpaceEconSim/SceneTraversals.cpp
@@ -5,27 +5,22 @@
 
 void SceneManager::GotoGameScene()
 {
-	std::cout << "Beginning simulation." << std::endl;
-	if(!m_pGameInst->Start())
-		std::cout << "	Creating new world..." << std::endl;
+	m_pGameInst->Start();
 	EnableSceneByID(SCENE_GAMEINST);
 }
 
 void SceneManager::GotoOptionsScene()
 {
-	std::cout << "Going to the options menu." << std::endl;
+	//
 }
 
 void SceneManager::Quit()
 {
-	std::cout << "Exiting the system, goodbye." << std::endl;
 	m_IsQuittingNextUpdate = true;
 }
 
 void SceneManager::GotoMenuScene()
 {
-	std::cout << "Returning to main menu." << std::endl;
 	m_pGameInst->Stop();
-	std::cout << "	Halting world..." << std::endl;
 	EnableSceneByID(SCENE_MAINMENU);
 }
