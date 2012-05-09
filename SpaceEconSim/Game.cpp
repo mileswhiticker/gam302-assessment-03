9d5fa47b858351d52cf3c24f38ec629e2ab01527
mwhiticker <miles_whiticker@gmail.com>
2012-05-10 05:25:24 +1000

    some files
    Signed-off-by: mwhiticker <miles_whiticker@gmail.com>

--- SpaceEconSim/Game.cpp
+++ SpaceEconSim/Game.cpp
@@ -1,13 +1,15 @@
 #include "Game.hpp"
+#include "Trader.hpp"
 
 #include <iostream>
 #include <sstream>
 
-#define MIN_STARSYSTEMS 5
-#define MAX_STARSYSTEMS 10
+#define MIN_STARSYSTEMS 1
+#define MAX_STARSYSTEMS 5
 
 Game::Game()
 :	m_Initialised(false)
+,	TimePassageRate(1)
 {
 	//
 }
@@ -17,7 +19,7 @@ bool Game::CheckInitialised()
 	return m_Initialised;
 }
 
-void Game::CreateNewWorld()
+void Game::CreateNewWorld(SelectListener* a_pSelectListener)
 {
 	m_Initialised = true;
 
@@ -25,7 +27,7 @@ void Game::CreateNewWorld()
 	short numStarSystems = rand()%MAX_STARSYSTEMS + MIN_STARSYSTEMS;
 	for(short i=0;i<numStarSystems;i++)
 	{
-		StarSystems.push_back(StarSystem());
+		StarSystems.push_back(StarSystem(*this, a_pSelectListener));
 	}
 }
 
@@ -34,13 +36,17 @@ void Game::Update(float a_Dt)
 	m_Initialised = false;
 	std::stringstream out;
 	out << "Game Update, fps: " << 1/a_Dt;
-	std::cout << out.str().c_str() << std::endl;
+	//std::cout << out.str().c_str() << std::endl;
 
+	//update all star systems out there
 	for(unsigned short i=0;i<StarSystems.size();i++)
 	{
-		StarSystems[i].Update(a_Dt);
+		StarSystems[i].Update(a_Dt, TimePassageRate);
 	}
 
-	//lotsa stuff
-	//
+	//update any traders in interstellar space
+	for(unsigned short i=0;i<InterstellarTraders.size();i++)
+	{
+		InterstellarTraders[i]->Update(a_Dt, TimePassageRate);
+	}
 }
