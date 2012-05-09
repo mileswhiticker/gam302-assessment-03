fee754daf2e8a9a1638fc4f4c8d5cbbe4120b006
mwhiticker <miles_whiticker@gmail.com>
2012-05-10 05:24:59 +1000

    some files
    Signed-off-by: mwhiticker <miles_whiticker@gmail.com>

--- SpaceEconSim/GUI_SystemDetail.cpp
+++ SpaceEconSim/GUI_SystemDetail.cpp
@@ -1,11 +1,62 @@
 #include "GameInst.hpp"
+#include "Helpers.hpp"
+#include "GameHelpers.hpp"
+#include "StarSystem.hpp"
+#include "Planet.hpp"
 
 void GameInst::SetupSystemDetailGUI()
 {
-	//
+	//create windows
+	m_pSystemView = sfg::Table::Create();
+	m_pSelectedSystem = NULL;
+
+	//create table cells
+	for(unsigned short i = 0; i < MAX_SYSTEMOBJECTS + 2; i++)
+	{
+		SystemDetailCells.push_back(sfg::Label::Create("system detail cell " + Num2Str(i + 1)));
+		m_pSystemView->Attach(SystemDetailCells.back(), sf::Rect<sf::Uint32>(1, i + 1, 1, 1));
+		//SystemDetailCells.back()->OnLeftClick.Connect(&Planet::SelectMe, a_pSystem);
+	}
+
+	//asteroid is a speshul case
+	//SystemListItems[a_pSystem->SystemUID]->OnLeftClick.Connect(&StarSystem::SelectMe, a_pSystem);
 }
 
 void GameInst::UpdateSystemDetailGUI()
 {
-	//
+	if(m_pSelectedSystem && m_CurView == VIEW_SYSTEM)
+	{
+		//nothing to see here, folks
+		SystemDetailCells[0]->SetText("Star name: " + m_pSelectedSystem->m_StarName);
+		SystemDetailCells[1]->SetText("Asteroid belt");
+
+		//update list of planets
+		int numPlanets = m_pSelectedSystem->Planets.size();
+		for(unsigned short n=2; n<numPlanets + 2; n++)
+		{
+			SystemDetailCells[n]->SetText( GetObjTypeAsStr(m_pSelectedSystem->Planets[n-2].MyType) + ": " + m_pSelectedSystem->Planets[n-2].m_Name );
+			SystemDetailCells[n]->OnLeftClick.Connect( &Planet::SelectMe, &m_pSelectedSystem->Planets[n-2] );
+		}
+
+		//clear other cells
+		int numEmptyCells = MAX_SYSTEMOBJECTS - numPlanets;
+		for(unsigned short n = numPlanets + 2; n < numEmptyCells + numPlanets + 2; n++)
+		{
+			SystemDetailCells[n]->SetText("");
+		}
+	}
+	else
+	{
+		for(unsigned short n=0;n<SystemDetailCells.size();n++)
+		{
+			SystemDetailCells[n]->SetText("");
+		}
+	}
+
+	//update the cells
+	sfg::Container::WidgetsList cells = m_pSystemView->GetChildren();
+	for(unsigned short n=0;n<cells.size();n++)
+	{
+		cells[n]->Update(0);
+	}
 }
--- SpaceEconSim/GUI_TraderDetail.cpp
+++ SpaceEconSim/GUI_TraderDetail.cpp
@@ -5,66 +5,88 @@
 
 void GameInst::SetupTraderDetailGUI()
 {
-	m_pDetailView->Add(m_pTraderView);
-	//column 1
-	m_pTraderView->Attach(m_pTraderName, sf::Rect<sf::Uint32>(1,1,1,1));
-	m_pTraderView->Attach(m_pTraderWealth, sf::Rect<sf::Uint32>(1,2,1,1));
-	m_pTraderView->Attach(m_pDecisiveness, sf::Rect<sf::Uint32>(1,3,1,1));
-	m_pTraderView->Attach(m_pRiskTaker, sf::Rect<sf::Uint32>(1,4,1,1));
-	m_pTraderView->Attach(m_pCautious, sf::Rect<sf::Uint32>(1,5,1,1));
-	m_pTraderView->Attach(m_pAmbitious, sf::Rect<sf::Uint32>(1,6,1,1));
-	m_pTraderView->Attach(m_pPride, sf::Rect<sf::Uint32>(1,7,1,1));
-	m_pTraderView->Attach(m_pRestlessness, sf::Rect<sf::Uint32>(1,8,1,1));
-	//column2
-	m_pTraderView->Attach(m_pShipName, sf::Rect<sf::Uint32>(2,1,1,1));
-	m_pTraderView->Attach(m_pFuel, sf::Rect<sf::Uint32>(2,2,1,1));
-	m_pTraderView->Attach(m_pStorage, sf::Rect<sf::Uint32>(2,3,1,1));
-	m_pTraderView->Attach(m_pMaintenance, sf::Rect<sf::Uint32>(2,4,1,1));
-	m_pTraderView->Attach(m_pSpeed, sf::Rect<sf::Uint32>(2,5,1,1));
-	m_pTraderView->Attach(m_pRange, sf::Rect<sf::Uint32>(2,6,1,1));
-	m_pTraderView->Attach(m_pLocation, sf::Rect<sf::Uint32>(2,7,1,1));
-	m_pTraderView->Attach(m_pDestination, sf::Rect<sf::Uint32>(2,8,1,1));
-	//column3
-	for(unsigned short n=0;n<8;n++)
+	//create windows
+	m_pTraderView = sfg::Table::Create();
+	m_pSelectedTrader = NULL;
+
+	//create table cells
+	for(unsigned short i=0;i<3;i++)
 	{
-		m_pTraderView->Attach(StorageSlots[n], sf::Rect<sf::Uint32>(3,n+1,1,1) );
+		//StorageSlots.push_back(sfg::Label::Create("storage slot " + Num2Str(i+1)));
+		for(unsigned short j=0;j<8;j++)
+		{
+			TraderDetailCells.push_back(sfg::Label::Create("trader detail cell " + Num2Str(8*i + j + 1)));
+			m_pTraderView->Attach(TraderDetailCells.back(), sf::Rect<sf::Uint32>(i + 1, j + 1, 1, 1));
+		}
 	}
 }
 
 void GameInst::UpdateTraderDetailGUI()
 {
-	if(m_pSelectedTrader)
+	if(m_pSelectedTrader && m_CurView == VIEW_TRADER)
 	{
-		m_pTraderName->SetText(m_pSelectedTrader->Name);
-		m_pTraderWealth->SetText( Num2Str(m_pSelectedTrader->Money));
-		m_pDecisiveness->SetText( Num2Str(m_pSelectedTrader->Decisiveness));
-		m_pRiskTaker->SetText( Num2Str(m_pSelectedTrader->RiskTaker));
-		m_pCautious->SetText( Num2Str(m_pSelectedTrader->Caution));
-		m_pAmbitious->SetText( Num2Str(m_pSelectedTrader->Ambition));
-		m_pPride->SetText( Num2Str(m_pSelectedTrader->Pride));
-		m_pRestlessness->SetText( Num2Str(m_pSelectedTrader->Restlessness));
-		//
-		m_pShipName->SetText( std::string(m_pSelectedTrader->Ship.Name + " (" + Num2Str(m_pSelectedTrader->Ship.Type) + ")"));
-		m_pFuel->SetText( Num2Str(m_pSelectedTrader->Ship.FuelStored) + "/" + Num2Str(m_pSelectedTrader->Ship.FuelMax));
-		m_pStorage->SetText( Num2Str(m_pSelectedTrader->Ship.StorageUsed) + "/" + Num2Str(m_pSelectedTrader->Ship.StorageMax));
-		m_pMaintenance->SetText( Num2Str(m_pSelectedTrader->Ship.MaintLevel) + "/" + Num2Str(m_pSelectedTrader->Ship.MaintMax));
-		m_pSpeed->SetText( Num2Str(m_pSelectedTrader->Ship.Speed));
-		m_pRange->SetText( Num2Str(m_pSelectedTrader->Ship.GetRange()));
-		m_pLocation->SetText( Num2Str(m_pSelectedTrader->LocationUID));
-		m_pDestination->SetText( Num2Str(m_pSelectedTrader->DestinationUID));
-		//
-		short numResources = m_pSelectedTrader->Ship.StoredResources.size();
+		//column1
+		TraderDetailCells[NAME]->SetText( "Trader name: " + m_pSelectedTrader->Name);
+		TraderDetailCells[WEALTH]->SetText( "Wealth: $" + Num2Str(m_pSelectedTrader->Money));
+		TraderDetailCells[DECISIVENESS]->SetText( "Decisiveness: " + Num2Str(m_pSelectedTrader->Decisiveness));
+		TraderDetailCells[RISKTAKER]->SetText( "Risk Taker: " + Num2Str(m_pSelectedTrader->RiskTaker));
+		TraderDetailCells[CAUTIOUS]->SetText( "Catiousness: " + Num2Str(m_pSelectedTrader->Caution));
+		TraderDetailCells[AMBITIOUS]->SetText( "Ambition: " + Num2Str(m_pSelectedTrader->Ambition));
+		TraderDetailCells[PRIDE]->SetText( "Pride: " + Num2Str(m_pSelectedTrader->Pride));
+		TraderDetailCells[RESTLESSNESS]->SetText( "Restlessness: " + Num2Str(m_pSelectedTrader->Restlessness));
+		//column2
+		TraderDetailCells[SHIPNAME]->SetText( "Ship name: " + std::string( m_pSelectedTrader->MyShip.Name + " (" + GetShipTypeAsStr(m_pSelectedTrader->MyShip.MyType) + ")") );
+		TraderDetailCells[FUEL]->SetText( "Fuel stores: " + Num2Str(m_pSelectedTrader->MyShip.FuelStored) + "/" + Num2Str(m_pSelectedTrader->MyShip.FuelMax));
+		TraderDetailCells[STORAGE]->SetText( "Used storage space: " + Num2Str(m_pSelectedTrader->MyShip.StorageUsed) + "/" + Num2Str(m_pSelectedTrader->MyShip.StorageMax));
+		TraderDetailCells[MAINTENANCE]->SetText( "Maintenance level: " + Num2Str(m_pSelectedTrader->MyShip.MaintLevel) + "/" + Num2Str(m_pSelectedTrader->MyShip.MaintMax));
+		TraderDetailCells[SPEED]->SetText( "Ship speed: " + Num2Str(m_pSelectedTrader->MyShip.Speed));
+		TraderDetailCells[RANGE]->SetText( "Travel range: " + Num2Str(m_pSelectedTrader->MyShip.GetRange()));
+		TraderDetailCells[LOCATION]->SetText( "Current location: " + Num2Str(m_pSelectedTrader->LocationUID));
+		TraderDetailCells[DESTINATION]->SetText( "Destination: " + Num2Str(m_pSelectedTrader->DestinationUID));
+		//column3
+		short numResources = m_pSelectedTrader->MyShip.StoredResources.size();
 		for(unsigned short n=0;n<numResources;n++)
 		{
-			StorageSlots[n]->SetText(GetResourceAsStr((RESOURCE_TYPE)(n+1)) + ": " + Num2Str(m_pSelectedTrader->Ship.StoredResources[(RESOURCE_TYPE)n+1]));
+			TraderDetailCells[TraderCells(n)]->SetText(GetResourceAsStr((RESOURCE_TYPE)(n+1)) + ": " + Num2Str(m_pSelectedTrader->MyShip.StoredResources[(RESOURCE_TYPE)n+1]));
+		}
+
+		/*
+		m_pTraderName->SetText( "Trader name: " + m_pSelectedTrader->Name);
+		m_pTraderWealth->SetText( "Wealth: $" + Num2Str(m_pSelectedTrader->Money));
+		m_pDecisiveness->SetText( "Decisiveness: " + Num2Str(m_pSelectedTrader->Decisiveness));
+		m_pRiskTaker->SetText( "Risk Taker: " + Num2Str(m_pSelectedTrader->RiskTaker));
+		m_pCautious->SetText( "Catiousness: " + Num2Str(m_pSelectedTrader->Caution));
+		m_pAmbitious->SetText( "Ambition: " + Num2Str(m_pSelectedTrader->Ambition));
+		m_pPride->SetText( "Pride: " + Num2Str(m_pSelectedTrader->Pride));
+		m_pRestlessness->SetText( "Restlessness: " + Num2Str(m_pSelectedTrader->Restlessness));
+		//
+		m_pShipName->SetText( "Ship name: " + std::string( m_pSelectedTrader->MyShip.Name + " (" + GetShipTypeAsStr(m_pSelectedTrader->MyShip.MyType) + ")") );
+		m_pFuel->SetText( "Fuel stores: " + Num2Str(m_pSelectedTrader->MyShip.FuelStored) + "/" + Num2Str(m_pSelectedTrader->MyShip.FuelMax));
+		m_pStorage->SetText( "Used storage space: " + Num2Str(m_pSelectedTrader->MyShip.StorageUsed) + "/" + Num2Str(m_pSelectedTrader->MyShip.StorageMax));
+		m_pMaintenance->SetText( "Maintenance level: " + Num2Str(m_pSelectedTrader->MyShip.MaintLevel) + "/" + Num2Str(m_pSelectedTrader->MyShip.MaintMax));
+		m_pSpeed->SetText( "Ship speed: " + Num2Str(m_pSelectedTrader->MyShip.Speed));
+		m_pRange->SetText( "Travel range: " + Num2Str(m_pSelectedTrader->MyShip.GetRange()));
+		m_pLocation->SetText( "Current location: " + Num2Str(m_pSelectedTrader->LocationUID));
+		m_pDestination->SetText( "Destination: " + Num2Str(m_pSelectedTrader->DestinationUID));
+		//
+		short numResources = m_pSelectedTrader->MyShip.StoredResources.size();
+		for(unsigned short n=0;n<numResources;n++)
+		{
+			StorageSlots[n]->SetText(GetResourceAsStr((RESOURCE_TYPE)(n+1)) + ": " + Num2Str(m_pSelectedTrader->MyShip.StoredResources[(RESOURCE_TYPE)n+1]));
 		}
 		for(unsigned short n=8-numResources;n<8;n++)
 		{
 			StorageSlots[n]->SetText("");
 		}
+		*/
 	}
 	else
 	{
+		for(unsigned short i=0;i<TraderDetailCells.size();i++)
+		{
+			TraderDetailCells[TraderCells(i)]->SetText("");
+		}
+		/*
 		//column 1
 		m_pTraderName->SetText("");
 		m_pTraderWealth->SetText("");
@@ -88,5 +110,13 @@ void GameInst::UpdateTraderDetailGUI()
 		{
 			StorageSlots[n]->SetText("");
 		}
+		*/
+	}
+
+	//update the cells
+	sfg::Container::WidgetsList cells = m_pTraderView->GetChildren();
+	for(unsigned short n=0;n<cells.size();n++)
+	{
+		cells[n]->Update(0);
 	}
 }
